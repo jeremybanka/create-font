53527f0c4b12ff1e6e60ec634c8d0203453a3c7a
Jeremy Banka <hello@jeremybanka.com>
2026-07-13 22:55:26 -0700

    👷 follow tooling best practices for checks/linters

--- packages/editor/eslint.config.ts
+++ packages/editor/eslint.config.ts
@@ -1,18 +1,30 @@
 import parser from "@typescript-eslint/parser"
+import atomIO from "atom.io/eslint-plugin"
 import type { Linter } from "eslint"
 import lasertag from "lasertag/eslint-plugin"
 
 export default [
 	{
-		files: ["src/**/*.tsx"],
+		files: ["src/**/*.{ts,tsx}"],
 		languageOptions: {
 			parser,
 			parserOptions: {
 				ecmaFeatures: { jsx: true },
 				ecmaVersion: "latest",
+				projectService: true,
 				sourceType: "module",
+				tsconfigRootDir: new URL(".", import.meta.url).pathname,
 			},
 		},
+		plugins: { "atom.io": atomIO },
+		rules: {
+			"atom.io/exact-catch-types": "error",
+			"atom.io/explicit-state-types": "error",
+			"atom.io/naming-convention": "error",
+		},
+	},
+	{
+		files: ["src/**/*.tsx"],
 		plugins: { lasertag },
 		rules: {
 			"lasertag/access-css-module-class-only": "error",
